f5a632d9c8ae43e40a3b25c636016efa5c409522
finamaulidiyah19 <56226660+finamaulidiyah19@users.noreply.github.com>
2021-10-25 22:41:46 +0700

    Add files via upload

--- database/topsis1.sql
+++ database/topsis1.sql
@@ -1,1 +1,170 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 25, 2021 at 05:10 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `topsis1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tab_alternatif`
+--
+
+CREATE TABLE `tab_alternatif` (
+  `id_alternatif` varchar(10) NOT NULL,
+  `nama_alternatif` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tab_alternatif`
+--
+
+INSERT INTO `tab_alternatif` (`id_alternatif`, `nama_alternatif`) VALUES
+('1', 'Ana'),
+('2', 'Ani'),
+('3', 'Marta'),
+('4', 'Sobirin'),
+('5', 'Sulaiman');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tab_kriteria`
+--
+
+CREATE TABLE `tab_kriteria` (
+  `id_kriteria` varchar(10) NOT NULL,
+  `nama_kriteria` varchar(50) NOT NULL,
+  `bobot` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tab_kriteria`
+--
+
+INSERT INTO `tab_kriteria` (`id_kriteria`, `nama_kriteria`, `bobot`) VALUES
+('1', 'Usia', 2),
+('2', 'Pend Akhir', 2),
+('3', 'Nilai Fisik', 4),
+('4', 'Nilai Akademik', 4),
+('5', 'Nilai Wawancara', 3),
+('6', 'Nilai Karakter', 3),
+('7', 'Nilai Prestasi', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tab_poin`
+--
+
+CREATE TABLE `tab_poin` (
+  `id_poin` varchar(10) NOT NULL,
+  `poin` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tab_poin`
+--
+
+INSERT INTO `tab_poin` (`id_poin`, `poin`) VALUES
+('1', '1'),
+('2', '2'),
+('3', '3'),
+('4', '4');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tab_topsis`
+--
+
+CREATE TABLE `tab_topsis` (
+  `id_alternatif` varchar(10) NOT NULL,
+  `id_kriteria` varchar(10) NOT NULL,
+  `nilai` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tab_topsis`
+--
+
+INSERT INTO `tab_topsis` (`id_alternatif`, `id_kriteria`, `nilai`) VALUES
+('1', '1', 3),
+('1', '2', 1),
+('1', '3', 3),
+('2', '1', 3),
+('2', '2', 2),
+('2', '3', 3),
+('3', '1', 1),
+('3', '2', 1),
+('3', '3', 2),
+('1', '4', 2),
+('4', '3', 1),
+('4', '4', 2),
+('4', '2', 4),
+('4', '1', 1),
+('2', '4', 3),
+('3', '4', 1),
+('1', '5', 2),
+('1', '6', 2),
+('1', '7', 2),
+('2', '5', 2),
+('2', '6', 2),
+('2', '7', 2),
+('3', '5', 4),
+('3', '6', 4),
+('3', '7', 3),
+('4', '5', 2),
+('4', '6', 4),
+('4', '7', 3),
+('5', '1', 1),
+('5', '2', 4),
+('5', '3', 4),
+('5', '4', 1),
+('5', '5', 4),
+('5', '6', 1),
+('5', '7', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tab_alternatif`
+--
+ALTER TABLE `tab_alternatif`
+  ADD PRIMARY KEY (`id_alternatif`);
+
+--
+-- Indexes for table `tab_kriteria`
+--
+ALTER TABLE `tab_kriteria`
+  ADD PRIMARY KEY (`id_kriteria`);
+
+--
+-- Indexes for table `tab_poin`
+--
+ALTER TABLE `tab_poin`
+  ADD PRIMARY KEY (`id_poin`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
